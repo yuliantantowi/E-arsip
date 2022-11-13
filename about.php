98d5642137bdc6db63e95f8c0d8c70d3e7b91642
yuliantantowi <118100220+yuliantantowi@users.noreply.github.com>
2022-11-13 16:47:42 +0700

    Create about.php

---
+++ about.php
@@ -0,0 +1,28 @@
+<?php include("sidebar/sidebar.php"); ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About</title>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+	<div class="container col-md-11">
+		<br>
+		<h2>About</h2><br>
+		<div class="row">
+             <div class="col-4">
+                   <img src="assets/poto.jpeg" style="width: 300px; height:400px;">    
+                </div>
+                <div class="col-8">
+                   <h3>Aplikasi ini dibuat oleh:</h3>
+                   <h5>Nama: YULIAN TANTOWI</h5>
+                   <h5>NIM : 203173047</h5>
+                   <h5>Tanggal: 1 April 2001</h5>
+                </div>
+            </div>
+        </div>
+	</div>
+</body>
+</html>
